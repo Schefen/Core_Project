74b886a7fc3f699f3c1625b88608b92f144fd4e4
Halil_İbrahim_Morgil <SchefenTR@gmail.com>
2023-08-26 15:40:45 +0300

    Add and Edit Transactions

--- Core_Project/Controllers/SkillController.cs
+++ Core_Project/Controllers/SkillController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Project.Controllers
@@ -9,8 +10,46 @@ namespace Core_Project.Controllers
         SkillManager _skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            //ViewBag.v1 = "Yetenek Sayfası";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Sayfası";
             var values = _skillManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            //ViewBag.v1 = "Yetenek Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            _skillManager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSkill(int id)
+        {
+            var value = _skillManager.TGetById(id);
+            _skillManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditSkill(int id)
+        {
+            //ViewBag.v1 = "Yetenek Güncelleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Güncelleme";
+            var value = _skillManager.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            _skillManager.TUpdate(skill);
+            return RedirectToAction("Index");
         }
     }
 }
--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface IGenericDal<T > where T : class
-    {
-        void Insert(T t);
-        void Delete(T t);
-        void Update(T t);
-        List<T> GetList();
-        T GetByID(int id); 
-    }
+	public interface IGenericDal<T> where T : class
+	{
+		void Insert(T t);
+		void Delete(T t);
+		void Update(T t);
+		List<T> GetList();
+		T GetByID(int id);
+	}
 }
