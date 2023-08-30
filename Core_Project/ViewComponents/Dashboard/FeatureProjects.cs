c26e5f9ba59a5c23453c8bbd03c3011e225fa60c
Halil_İbrahim_Morgil <SchefenTR@gmail.com>
2023-08-31 02:28:21 +0300

    Transactions on Dashboard again

---
+++ Core_Project/ViewComponents/Dashboard/FeatureProjects.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.ViewComponents.Dashboard
+{
+    public class FeatureProjects : ViewComponent
+    {
+        PortfolioManager _portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ Core_Project/ViewComponents/Dashboard/FeatureStatistics2.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.ViewComponents.Dashboard
+{
+    public class FeatureStatistics2 : ViewComponent
+    {
+        Context _context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = _context.Portfolios.Count();
+            ViewBag.v2 = _context.Messages.Count();
+            ViewBag.v3 = _context.Services.Count();
+            return View();
+        }
+    }
+}
