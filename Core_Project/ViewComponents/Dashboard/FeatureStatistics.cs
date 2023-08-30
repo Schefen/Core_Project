a4c44f3243a2d42f2ddf8ae17d21b05d4a69d899
Halil_İbrahim_Morgil <SchefenTR@gmail.com>
2023-08-30 23:58:22 +0300

    Transactions on Dashboard part1

---
+++ Core_Project/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        Context _context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = _context.Skills.Count();
+            ViewBag.v2 = _context.Messages.Where(x => x.MessageStatus == false).Count();
+            ViewBag.v3 = _context.Messages.Where(x => x.MessageStatus == true).Count();
+            ViewBag.v4 = _context.Experiences.Count();
+            return View();
+
+        }
+    }
+}
