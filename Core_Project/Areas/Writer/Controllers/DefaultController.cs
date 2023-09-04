4a1817eb4280d07f5b559f6f3697eabdec0449f2
Halil_İbrahim_Morgil <SchefenTR@gmail.com>
2023-09-04 21:29:36 +0300

    Announcements

--- Core_Project/Areas/Writer/Controllers/DefaultController.cs
+++ Core_Project/Areas/Writer/Controllers/DefaultController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Project.Areas.Writer.Controllers
@@ -7,9 +10,16 @@ namespace Core_Project.Areas.Writer.Controllers
     [Authorize]
     public class DefaultController : Controller
     {
+        AnnouncementManager _announcementManager = new AnnouncementManager(new EfAnnouncementDal());
         public IActionResult Index()
         {
-            return View();
+            var values = _announcementManager.TGetList();
+            return View(values);
+        }
+        public IActionResult AnnouncementDetails(int id)
+        {
+            Announcement announcement = _announcementManager.TGetById(id);
+            return View(announcement);
         }
     }
 }
---
+++ EntityLayer/Concrete/Announcement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Announcement
+    {
+        [Key]
+        public int AnnouncementId { get; set; }
+        public string AnnouncementTitle { get; set; }
+        public DateTime AnnouncementDate { get; set; }
+        public string AnnouncementStatus { get; set; }
+        public string AnnouncementContent { get; set; }
+    }
+}
