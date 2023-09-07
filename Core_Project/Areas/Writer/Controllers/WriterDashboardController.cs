ef4600483d86c4baf11d8608d253bc36150b9be7
Halil_İbrahim_Morgil <SchefenTR@gmail.com>
2023-09-07 19:53:44 +0300

    Some changes on dashboard

---
+++ Core_Project/Areas/Writer/Controllers/WriterDashboardController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Core_Project.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class WriterDashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public WriterDashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.WriterUserName + " " + values.WriterUserSurname;
+            // Weather Api
+            string apikey = "a25c24328f096c5e2c2e088c8e15992e";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + apikey;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v5 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            //statistics
+            Context context = new Context();
+            ViewBag.v1 = 0;
+            ViewBag.v2 = context.Announcements.Count();
+            ViewBag.v3 = 0;
+            ViewBag.v4 = context.Skills.Count();
+
+            return View();
+        }
+    }
+}
---
+++ Core_Project/Areas/Writer/ViewComponents/Notification.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Areas.Writer.ViewComponents
+{
+    public class Notification : ViewComponent
+    {
+        AnnouncementManager _announcementManager = new AnnouncementManager(new EfAnnouncementDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _announcementManager.TGetList();
+            return View(values);
+        }
+    }
+}
